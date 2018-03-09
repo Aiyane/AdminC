acbcd1e24a79e9e3dfc47b78a48ba0c9273d7d52
aiyane <2310091880@qq.com>
2018-03-09 14:17:59 +0800

    增加注释

--- include/func.h
+++ include/func.h
@@ -2,21 +2,21 @@
 #define __FUNC_H__
 #include "model.h"
 
-int store(User *a);
-void check();
-int checkUser(int num, char *pwd);
-User *getUser(int num);
-int startLogin(User *a);
-int endLogin(User *a);
-int delUser(int num);
-int moneyAdd(int num, int money);
-int moneyRefund(int num, int money);
-void standShow();
-void initStand();
-void addStand(int num);
-void changeStand(int num);
-void moneyCheck();
-void userMoney(int num);
-int standDel(int num);
+int store(User *a);                 // 储存用户
+void check();                       // 查看全部用户信息
+int checkUser(int num, char *pwd);  // 检查用户是否存在
+User *getUser(int num);             // 获得用户
+int startLogin(User *a);            // 上机
+int endLogin(User *a);              // 下机
+int delUser(int num);               // 删除用户
+int moneyAdd(int num, int money);   // 充值
+int moneyRefund(int num, int money);// 退费
+void standShow();                   // 显示标准
+void initStand();                   // 初始化标准
+void addStand(int num);             // 增加标准
+void changeStand(int num);          // 改变标准
+void moneyCheck();                  // 检查总营业额
+void userMoney(int num);            // 检查用户消费金额
+int standDel(int num);              // 删除标准
 
 #endif
